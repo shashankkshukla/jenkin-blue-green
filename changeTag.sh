f82689b2edea41f34ca325753060f55fb6ac7391
shashankkshukla <shashank.shashank@gmail.com>
2020-08-24 18:46:50 +0530

    Update changeTag.sh

--- changeTag.sh
+++ changeTag.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed "s/tagVersion/$1/g"  /var/lib/jenkins/workspace/pipeline-test/ingress/helloworld-v2.yml >  /var/lib/jenkins/workspace/pipeline-test/ingress/helloworld-v2-tag.yml
+sed "s/tagVersion/$1/g"  /var/lib/jenkins/workspace/pipeline-test/ingress/nodeapp-v2.yml >  /var/lib/jenkins/workspace/pipeline-test/ingress/nodeapp-v2-tag.yml
